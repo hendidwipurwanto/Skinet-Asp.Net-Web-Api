80f876e18032a8a6f97e505fe930f4b989b35a1c
hendidwipurwanto <hendi.purwanto@gustodian.com>
2025-03-02 14:35:30 +0700

    Add project files.

---
+++ API/Controllers/ProductsController.cs
@@ -0,0 +1,85 @@
+﻿using Core.Entities;
+using Infrastructure.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        private readonly StoreContext _context;
+        public ProductsController(StoreContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Product>>> GetProducts()
+        {
+            return await _context.products.ToListAsync();
+           
+        }
+
+        // GET api/<ProductsController>/5
+        [HttpGet("{id:int}")]
+        public async Task<ActionResult<Product>> GetProduct(int id)
+        {
+            var product = await _context.products.FindAsync(id);
+
+            if(product == null)
+            {
+                return NotFound();
+            }
+            return product;
+        }
+
+       
+        [HttpPost]
+        public async Task<ActionResult<Product>> CreateProduct(Product product)
+        {
+            _context.products.Add(product);
+            await _context.SaveChangesAsync();
+
+            return product;
+        }
+
+        // PUT api/<ProductsController>/5
+        [HttpPut("{id:int}")]
+        public async Task<ActionResult>  UpdateProduct(int id, Product product)
+        {
+            if (product.Id != id || !ProductExist(id))
+            {
+                return BadRequest("Cannot update this product");
+            }
+            _context.Entry(product).State = EntityState.Modified;
+
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool ProductExist(int id)
+        {
+            return _context.products.Any(x => x.Id == id);
+        }
+
+             
+        [HttpDelete("{id:int}")]
+        public async Task<ActionResult> DeleteProduct(int id)
+        {
+            var product = await _context.products.FindAsync(id);
+            if(product== null)
+            {
+                return NotFound();            
+            }
+            _context.products.Remove(product);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+    }
+}
---
+++ Infrastructure/Data/StoreContext.cs
@@ -0,0 +1,26 @@
+﻿using Core.Entities;
+using Infrastructure.Config;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Data
+{
+    public class StoreContext:DbContext
+    {
+        public StoreContext(DbContextOptions options ) : base(options)
+        {                   
+        }
+        public DbSet<Product> products { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(ProductConfiguration).Assembly);
+        }
+    }
+}
