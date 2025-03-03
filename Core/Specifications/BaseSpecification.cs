462cfd3961d7ccafb2c7256249b09a00171a562e
hendidwipurwanto <hendi.purwanto@gustodian.com>
2025-03-03 15:42:48 +0700

    Setting up the specification classes

---
+++ Core/Specifications/BaseSpecification.cs
@@ -0,0 +1,20 @@
+﻿using Core.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Specifications
+{
+    public class BaseSpecification<T>: ISpecification<T>
+    {
+        private readonly Expression<Func<T, bool>> criteria;
+        public BaseSpecification(Expression<Func<T, bool>> criteria)
+        {
+            this.criteria = criteria;
+        }
+        public Expression<Func<T, bool>> Criteria => criteria;
+    }
+}
